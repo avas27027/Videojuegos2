1fb6c9689fa970d4f702844b1cdfccdcc74b624e
avas27 <alvas27027@users.noreply.github.com>
2022-05-20 14:40:57 -0500

    3p

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossController : MonoBehaviour
+{
+    public float maxHealth;
+    public Slider healthbar;
+    public Slider powerbar;
+
+    private float mHealth;
+
+    private void Start()
+    {
+        mHealth = maxHealth;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fireball"))
+        {
+            // Hubo una colision
+            mHealth -= maxHealth * 0.25f;
+            healthbar.value -= 0.25f;
+            powerbar.value += 0.25f;
+
+
+            if (mHealth <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
